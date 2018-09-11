ddc965c215bf4346157ce80264ecc816f8a895bc
Toshihiro Kuboi <toshikuboi@hotmail.com>
2018-09-11 16:11:01 -0700

    add training data generator

--- darknet53_model.py
+++ darknet53_model.py
@@ -8,7 +8,13 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
+import re 
+import glob
+import numpy as np
 import tensorflow as tf
+from PIL import Image, ImageDraw
+from random import shuffle
 
 _BATCH_NORM_DECAY = 0.997
 _BATCH_NORM_EPSILON = 1e-5
@@ -16,6 +22,10 @@ DEFAULT_VERSION = 2
 DEFAULT_DTYPE = tf.float32
 CASTABLE_TYPES = (tf.float16,)
 ALLOWED_TYPES = (DEFAULT_DTYPE,) + CASTABLE_TYPES
+BATCH_SIZE = 100
+LIMIT = 1000
+IMAGE_SIZE = 256
+NUM_EPOCH = 10
 
 def batch_norm(inputs, training, data_format):
     return tf.layers.batch_normalization(
@@ -89,6 +99,13 @@ def darknet53_base(inputs, data_format):
     return inputs
 
 def darknet53(inputs, data_format):
+    # transpose the inputs to NCHW
+    if data_format == 'channels_first':
+        inputs = tf.transpose(inputs, [0, 3, 1, 2])
+
+    # normalize values to range [0..1]
+    inputs = inputs / 255
+
     inputs = darknet53_base(inputs, data_format)
     #shape = tf.shape(inputs)
     shape=inputs.get_shape().as_list()
@@ -100,13 +117,33 @@ def darknet53(inputs, data_format):
     #inputs = tf.nn.softmax(inputs, axis=None)
     return inputs
 
+def one_hot(label, num_classes):
+    vec = np.zeros(num_classes)
+    digit = re.find(r'\d+', label)
+    c = int(digit) - BASE 
+    vec[c]=1
+    return vec
 
-img = Image.open(filename)
-img_resized = img.resize(size=(256, 256))
+def get_image_list(directory):
+    images = []
+    classes = []
+    for subdir in next(os.walk(direcotry))[1]:
+        images += glob.glob(directory + '/' + subdir + '/*.jpeg')
+        classes += [subdir] * len(images)
+    return zip(images, classes)
+
+def get_images_labels(images_labels):
+    x = []
+    y = []
+    for image,label in image_labels:
+        img = Image.open(filename)
+        x.append(img.resize(size=(IMAGE_SIZE, IMAGE_SIZE)))
+        y.append(one_hot(label, 1000))
+    return np.array(x), np.array(y)
 
 # tf Graph Input
-inputs = tf.placeholder(tf.float32, [None, 256, 256, 3])
-y = tf.placeholder(tf.float32, [None, 10]) # 0-9 digits recognition => 10 classes
+inputs = tf.placeholder(tf.float32, [None, IMAGE_SIZE, IMAGE_SIZE, 3])
+y = tf.placeholder(tf.float32, [None, 1000]) # 0-9 digits recognition => 10 classes
 
 y_hat = darknet53(inputs, 'channels_first') 
 
@@ -116,6 +153,16 @@ optimizer = tf.train.AdamOptimizer(1e-4).minimize(cost)
 correct_prediction = tf.equal(tf.argmax(y_hat, 1), tf.argmax(y, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
+images_labels = get_image_list(directory)
+images_labels = shuffle(images_labels)
+images_labels, rest = images_labels[:LIMIT], images_labels[LIMIT:]
+
+total_batch = len(image_labels) / BATCH_SIZE
+
 with tf.Session() as sess:
      sess.run(tf.global_variables_initializer())
- 
+     for e in range(NUM_EPOCH):
+         for i in range(total_batch):
+             batch_x_y = image_labels[i*BATCH_SIZE:i*BATCH_SIZE+BATCH_SIZE]
+             images,labels = get_images_labels(batch_x_y) 
+             _, loss = sess.run([optimizer, cost], feed_dict={inputs: images, y: labels})             print loss 
